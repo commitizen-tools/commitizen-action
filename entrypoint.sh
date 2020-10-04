c9daba9a1c9824103a58916899cbbca327784d96
BobCashStory <47117399+BobCashStory@users.noreply.github.com>
2020-10-04 18:45:43 +0200

    refactor: bump system
    allow DRY_RUN CHANGELOG PRERELEASE work together

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-: "${INPUT_DRY_RUN:=false}"
+if [ $INPUT_DRY_RUN ]; then INPUT_DRY_RUN='--dry-run'; else INPUT_DRY_RUN=''; fi
+if [ $INPUT_CHANGELOG ]; then INPUT_CHANGELOG='--changelog'; else INPUT_CHANGELOG=''; fi
+if [ $PRERELEASE ]; then PRERELEASE="--prerelease $PRERELEASE"; else PRERELEASE=''; fi
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 # : "${INPUT_CHANGELOG:=true}" ignroed for now, let's check that it works
@@ -27,13 +29,10 @@ echo "Configuring git user and email..."
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
-if ! $INPUT_DRY_RUN; then
-    echo "Running cz..."
-    cz bump --yes --changelog
-else
-    echo "Running dry run cz..."
-    cz bump --yes --changelog --dry-run
-fi
+
+echo "Running cz: $INPUT_DRY_RUN $INPUT_CHANGELOG $PRERELEASE"
+cz bump --yes $INPUT_DRY_RUN $INPUT_CHANGELOG $PRERELEASE
+
 
 echo "Pushing to branch..."
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
