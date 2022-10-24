2bae08e03fd57872549974bc961dd0c5061ac762
Hendry, Adam <adam.hendry@metronic.com>
2022-10-24 12:50:48 -0700

    feat(debug): add option for debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,19 @@ git config --local pull.rebase true
 echo "Git name: $(git config --get user.name)"
 echo "Git email: $(git config --get user.email)"
 
+if [[ $INPUT_GPG_SIGN == 'true' ]]; then
+  if [[ -z $INPUT_GPG_PRIVATE_KEY ]]; then
+    echo 'Missing input "gpg_private_key".' >&2
+    exit 2
+  fi
+  echo "Configuring GPG for signing commits and tags..."
+  git config --local commit.gpgsign true
+  git config --local tag.gpgsign true
+  git config --local user.signingkey "${INPUT_GPG_PRIVATE_KEY}"
+  echo "Git sign commits?: $(git config --get commit.gpgsign)"
+  echo "Git sign tags?: $(git config --get tag.gpgsign)"
+fi
+
 PIP_CMD=('pip' 'install')
 if [[ $INPUT_COMMITIZEN_VERSION == 'latest' ]]; then
   PIP_CMD+=('commitizen')
@@ -29,19 +42,14 @@ echo "Commitizen version: $(cz version)"
 PREV_REV="$(cz version --project)"
 
 CZ_CMD=('cz')
+if [[ $INPUT_DEBUG == 'true' ]]; then
+  CZ_CMD+=('--debug')
+fi
 if [[ $INPUT_NO_RAISE ]]; then
   CZ_CMD+=('--no-raise' "$INPUT_NO_RAISE")
 fi
 CZ_CMD+=('bump' '--yes')
 if [[ $INPUT_GPG_SIGN == 'true' ]]; then
-  if [[ -z $INPUT_GPG_PRIVATE_KEY ]]; then
-    echo 'Missing input "gpg_private_key".' >&2
-    exit 2
-  fi
-  git config --local gpg.program gpg
-  git config --local commit.gpgsign true
-  git config --local tag.gpgsign true
-  git config --local user.signingkey "${INPUT_GPG_PRIVATE_KEY}"
   CZ_CMD+=('--gpg-sign')
 fi
 if [[ $INPUT_DRY_RUN == 'true' ]]; then
